4a5489f9d88d850f01ea0e6f861d4daebad0d9a5
VKnyshov <valeriy.knyshov@gmail.com>
2025-02-04 12:23:46 +0200

    fixed some bugs

--- posts/serializers.py
+++ posts/serializers.py
@@ -5,5 +5,5 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["id", "user", "text", "created_at", "updated_at"]  # ✅ Додано `updated_at`
-        read_only_fields = ["id", "user", "created_at", "updated_at"]  # ✅ `updated_at` оновлюється автоматично
+        fields = ["id", "user", "text", "created_at", "updated_at"]
+        read_only_fields = ["id", "user", "created_at", "updated_at"]
